b3407c10085c1f431d0099ce7a510bc72f215bf2
AAhmed233 <ahmeddiouri23@gmail.com>
2024-12-13 19:45:50 +0100

    python inheritance

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-from flask import Flask,render_template
+from flask import Flask,request,render_template,jsonify
 
+app = Flask(__name__)
+
+@app.route("/childa")
+def childa():
+    return render_template("child-A.html")
+
+@app.route("/childb")
+def childb():
+    return render_template("child-B.html")
+
+@app.route("/parent")
+def parent():
+    return render_template("parent.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
